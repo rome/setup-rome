10d07575e49cae7c41719ef5a76c61a7ff395902
Emanuele Stoppa <my.burning@gmail.com>
2022-03-28 14:15:14 +0100

    chore: format

--- src/index.js
+++ src/index.js
@@ -201,8 +201,9 @@ function _getTempDirectory() {
 module.exports = main;
 
 if (require.main === module) {
-	main().catch((err) => {
-		console.log(error);
-		process.exit(1);
-	});
+	main()
+		.catch((err) => {
+			console.log(error);
+			process.exit(1);
+		});
 }
