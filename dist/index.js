5c4db9e52cb61497be53878e8e94d092e165358e
Micha Reiser <micha@rome.tools>
2022-03-28 12:20:42 +0200

    Add Rome Format (#6)

--- dist/index.js
+++ dist/index.js
@@ -1,216 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 2932:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-const assert = __nccwpck_require__(9491);
-const fs = __nccwpck_require__(7147);
-const path = __nccwpck_require__(1017);
-const process = __nccwpck_require__(7282);
-
-const core = __nccwpck_require__(2186);
-const io = __nccwpck_require__(7436);
-const github = __nccwpck_require__(5438);
-const toolCache = __nccwpck_require__(7784);
-
-async function main() {
-	try {
-		const romePath = await resolveRome();
-
-		if (romePath == null) {
-			core.info("Rome is not installed, installing it now...");
-			try {
-				core.startGroup("Installing Rome");
-				await install();
-			} finally {
-				core.endGroup();
-			}
-		} else {
-			core.info(`Use pre-installed Rome ${romePath}`);
-		}
-	} catch (error) {
-		core.setFailed(error);
-	}
-}
-
-/**
- * Resolves the path to the rome binary.
- * @returns {PromiseLike<string | null>} the path to the Rome binary or `null` if Rome isn't installed
- */
-async function resolveRome() {
-	try {
-		return await io.which("rome", true);
-	} catch {
-		return null;
-	}
-}
-
-/**
- * Installs rome and adds it to the path.
- */
-async function install() {
-	// Create a temp directory because `addPath` adds the directory and not the binary to the path.
-	const romeDirectory = path.join(_getTempDirectory(), ".rome_bin");
-	const romeBinary = path.join(romeDirectory, `rome${getBinaryExtension()}`);
-
-	const tagName = await resolveReleaseTagName();
-	const url = await getDownloadUrl(tagName);
-
-	try {
-		core.debug(`Download tool from '${url}' to ${romeBinary}.`);
-		await toolCache.downloadTool(url, romeBinary);
-	} catch (error) {
-		const statusCode = error.httpStatusCode;
-		if (
-			typeof (statusCode) === "number" &&
-			statusCode >= 400 &&
-			statusCode < 500
-		) {
-			core.error(error);
-			const version = core.getInput("version");
-			const releaseUrl = `https://github.com/rome/tools/releases/${encodeURIComponent(
-				tagName,
-			)}`;
-
-			throw new Error(
-				`Failed to retrieve the binary for Rome version '${version}'. Is ${version} (${releaseUrl}) a valid (rusty-) Rome version?`,
-			);
-		}
-
-		core.error("Failed to retrieve the Rome binary from '${url}'.");
-
-		throw error;
-	}
-
-	if (process.platform == "linux" || process.platform == "darwin") {
-		fs.chmodSync(romeBinary, 0o755);
-	}
-
-	core.addPath(romeDirectory);
-}
-
-async function getDownloadUrl(tagName) {
-	const binaryName = `${getDownloadBinaryBaseName()}${getBinaryExtension()}`;
-
-	return `https://github.com/rome/tools/releases/download/${encodeURIComponent(
-		tagName,
-	)}/${encodeURIComponent(binaryName)}`;
-}
-
-async function resolveReleaseTagName() {
-	const version = core.getInput("version");
-
-	if (version == "latest") {
-		return "latest";
-	}
-
-	switch (version) {
-		case "latest":
-			return "latest";
-		case "preview":
-			return await resolveLatestPreviewVersion();
-		default:
-			return `v${version}`;
-	}
-}
-
-async function resolveLatestPreviewVersion() {
-	const token = core.getInput("github-token", { required: true });
-	const octokit = github.getOctokit(token);
-
-	const { repository } = await octokit.graphql(
-		`
-        { 
-            repository(owner: "rome", name: "tools") {
-                releases(orderBy: { field:CREATED_AT, direction:DESC }, first: 100) {
-                    nodes {
-                        isPrerelease
-                        tagName
-                    }
-                }
-            }
-        }`,
-		{},
-	);
-
-	const releases = repository?.releases?.nodes;
-
-	if (releases == null) {
-		throw new Error("Failed to retrieve the list of releases");
-	}
-
-	const firstPreRelease = releases.find((release) => release.isPrerelease);
-
-	if (firstPreRelease == null) {
-		core.error("Failed to retrieve pre-release, falling back to latest.");
-		return "latest";
-	}
-
-	core.info(`Resolved latest preview version to ${firstPreRelease.tagName}`);
-
-	return firstPreRelease.tagName;
-}
-
-function getDownloadBinaryBaseName() {
-	switch (process.platform) {
-		case "win32":
-		case "linux":
-		case "darwin":
-			const arch = getRunnerArchitecture();
-			return `rome-${process.platform}-${arch}`;
-
-		default:
-			throw new Error(`Unsupported platform ${process.platform}`);
-	}
-}
-
-function getRunnerArchitecture() {
-	const arch = process.env["RUNNER_ARCH"];
-	assert(
-		arch,
-		"Expected the arch to be exposed in the RUNNER_ARCH environment variable.",
-	);
-
-	switch (arch) {
-		case "X64":
-		case "ARM64":
-			return arch.toLocaleLowerCase();
-
-		default:
-			throw new Error(`Unsupported architecture ${arch}.`);
-	}
-}
-
-function getBinaryExtension() {
-	if (process.platform == "win32") {
-		return ".exe";
-	}
-	return "";
-}
-
-// Retrieves the temp directory. Copied from @actions/tool-cache
-function _getTempDirectory() {
-	const tempDirectory = process.env["RUNNER_TEMP"];
-
-	if (tempDirectory == null) {
-		core.warning(
-			"Temp directory not exposed via 'RUNNER_TEMP' environment variable. Uses current directory instead.",
-		);
-		return "";
-	}
-	return tempDirectory;
-}
-
-module.exports = main;
-
-if (require.main === require.cache[eval('__filename')]) {
-	main();
-}
-
-
-/***/ }),
-
 /***/ 7351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -12360,6 +12150,216 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 4351:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const assert = __nccwpck_require__(9491);
+const fs = __nccwpck_require__(7147);
+const path = __nccwpck_require__(1017);
+const process = __nccwpck_require__(7282);
+
+const core = __nccwpck_require__(2186);
+const io = __nccwpck_require__(7436);
+const github = __nccwpck_require__(5438);
+const toolCache = __nccwpck_require__(7784);
+
+async function main() {
+	try {
+		const romePath = await resolveRome();
+
+		if (romePath == null) {
+			core.info("Rome is not installed, installing it now...");
+			try {
+				core.startGroup("Installing Rome");
+				await install();
+			} finally {
+				core.endGroup();
+			}
+		} else {
+			core.info(`Use pre-installed Rome ${romePath}`);
+		}
+	} catch (error) {
+		core.setFailed(error);
+	}
+}
+
+/**
+ * Resolves the path to the rome binary.
+ * @returns {PromiseLike<string | null>} the path to the Rome binary or `null` if Rome isn't installed
+ */
+async function resolveRome() {
+	try {
+		return await io.which("rome", true);
+	} catch {
+		return null;
+	}
+}
+
+/**
+ * Installs rome and adds it to the path.
+ */
+async function install() {
+	// Create a temp directory because `addPath` adds the directory and not the binary to the path.
+	const romeDirectory = path.join(_getTempDirectory(), ".rome_bin");
+	const romeBinary = path.join(romeDirectory, `rome${getBinaryExtension()}`);
+
+	const tagName = await resolveReleaseTagName();
+	const url = await getDownloadUrl(tagName);
+
+	try {
+		core.debug(`Download tool from '${url}' to ${romeBinary}.`);
+		await toolCache.downloadTool(url, romeBinary);
+	} catch (error) {
+		const statusCode = error.httpStatusCode;
+		if (
+			typeof (statusCode) === "number" &&
+			statusCode >= 400 &&
+			statusCode < 500
+		) {
+			core.error(error);
+			const version = core.getInput("version");
+			const releaseUrl = `https://github.com/rome/tools/releases/${encodeURIComponent(
+				tagName,
+			)}`;
+
+			throw new Error(
+				`Failed to retrieve the binary for Rome version '${version}'. Is ${version} (${releaseUrl}) a valid (rusty-) Rome version?`,
+			);
+		}
+
+		core.error("Failed to retrieve the Rome binary from '${url}'.");
+
+		throw error;
+	}
+
+	if (process.platform == "linux" || process.platform == "darwin") {
+		fs.chmodSync(romeBinary, 0o755);
+	}
+
+	core.addPath(romeDirectory);
+}
+
+async function getDownloadUrl(tagName) {
+	const binaryName = `${getDownloadBinaryBaseName()}${getBinaryExtension()}`;
+
+	return `https://github.com/rome/tools/releases/download/${encodeURIComponent(
+		tagName,
+	)}/${encodeURIComponent(binaryName)}`;
+}
+
+async function resolveReleaseTagName() {
+	const version = core.getInput("version");
+
+	if (version == "latest") {
+		return "latest";
+	}
+
+	switch (version) {
+		case "latest":
+			return "latest";
+		case "preview":
+			return await resolveLatestPreviewVersion();
+		default:
+			return `v${version}`;
+	}
+}
+
+async function resolveLatestPreviewVersion() {
+	const token = core.getInput("github-token", { required: true });
+	const octokit = github.getOctokit(token);
+
+	const { repository } = await octokit.graphql(
+		`
+        { 
+            repository(owner: "rome", name: "tools") {
+                releases(orderBy: { field:CREATED_AT, direction:DESC }, first: 100) {
+                    nodes {
+                        isPrerelease
+                        tagName
+                    }
+                }
+            }
+        }`,
+		{},
+	);
+
+	const releases = repository?.releases?.nodes;
+
+	if (releases == null) {
+		throw new Error("Failed to retrieve the list of releases");
+	}
+
+	const firstPreRelease = releases.find((release) => release.isPrerelease);
+
+	if (firstPreRelease == null) {
+		core.error("Failed to retrieve pre-release, falling back to latest.");
+		return "latest";
+	}
+
+	core.info(`Resolved latest preview version to ${firstPreRelease.tagName}`);
+
+	return firstPreRelease.tagName;
+}
+
+function getDownloadBinaryBaseName() {
+	switch (process.platform) {
+		case "win32":
+		case "linux":
+		case "darwin":
+			const arch = getRunnerArchitecture();
+			return `rome-${process.platform}-${arch}`;
+
+		default:
+			throw new Error(`Unsupported platform ${process.platform}`);
+	}
+}
+
+function getRunnerArchitecture() {
+	const arch = process.env["RUNNER_ARCH"];
+	assert(
+		arch,
+		"Expected the arch to be exposed in the RUNNER_ARCH environment variable.",
+	);
+
+	switch (arch) {
+		case "X64":
+		case "ARM64":
+			return arch.toLocaleLowerCase();
+
+		default:
+			throw new Error(`Unsupported architecture ${arch}.`);
+	}
+}
+
+function getBinaryExtension() {
+	if (process.platform == "win32") {
+		return ".exe";
+	}
+	return "";
+}
+
+// Retrieves the temp directory. Copied from @actions/tool-cache
+function _getTempDirectory() {
+	const tempDirectory = process.env["RUNNER_TEMP"];
+
+	if (tempDirectory == null) {
+		core.warning(
+			"Temp directory not exposed via 'RUNNER_TEMP' environment variable. Uses current directory instead.",
+		);
+		return "";
+	}
+	return tempDirectory;
+}
+
+module.exports = main;
+
+if (require.main === require.cache[eval('__filename')]) {
+	main();
+}
+
+
+/***/ }),
+
 /***/ 2877:
 /***/ ((module) => {
 
@@ -12570,7 +12570,7 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(2932);
+/******/ 	var __webpack_exports__ = __nccwpck_require__(4351);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
--- test.sh
+++
@@ -1,7 +0,0 @@
-set -e;
-DIFF=$(diff ./dist/index.js ./dist/index.original.js)
- if [ "$DIFF" != "" ] then
-  echo "Please re-build the index.js file with 'npm run build' and commit the changed files in the 'dist' directory." >&2
-  echo "$DIFF" >&2
-  exit 1
-fi
